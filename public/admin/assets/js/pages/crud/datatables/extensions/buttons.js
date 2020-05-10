e7d74cfd31d909c5b22031a68d13dc844ac2d9a3
Leonardo Viada <leonardo.viada@revo.digital>
2020-05-10 23:20:09 +0200

    updated styling

--- public/admin/assets/js/pages/crud/datatables/extensions/buttons.js
+++ public/admin/assets/js/pages/crud/datatables/extensions/buttons.js
@@ -1,12 +1,79 @@
 "use strict";
 var KTDatatablesExtensionButtons = function() {
 
-    var initTable1 = function() {
+
+    var initTable4 = function() {
 
         // begin first table
+        var table = $('#kt_table_4').DataTable({
+            responsive: true,
+
+            buttons: [
+                'print',
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5',
+            ],
+            columnDefs: [
+                {
+                    targets: -1,
+                    title: 'Azioni',
+                    orderable: false,
+                    render: function(data, type, full, meta) {
+                        return '\
+					<span class="dropdown">\
+						<a href="#" class="btn btn-sm btn-clean btn-icon btn-icon-md" data-toggle="dropdown" aria-expanded="true">\
+						  <i class="la la-ellipsis-h"></i>\
+						</a>\
+						<div class="dropdown-menu dropdown-menu-right">\
+							<a class="dropdown-item" href="/admin/resps/edit/' + data + '"><i class="la la-edit"></i> Modifica</a>\
+                            <form action="/admin/resps/' + data + '" method="POST">\
+                                <input type="hidden" name="_method" value="DELETE">\
+                                    <a class="dropdown-item" type="submit">\
+                                        <button type="submit" class="btn btn-danger">Elimina</button>\
+                                    </a>\
+                            </form>\
+						</div>\
+					</span>';
+                    },
+                },
+            ],
+            processing: true,
+            serverSide: false
+        });
+
+        $('#export_print').on('click', function(e) {
+            e.preventDefault();
+            table.button(0).trigger();
+        });
+
+        $('#export_copy').on('click', function(e) {
+            e.preventDefault();
+            table.button(1).trigger();
+        });
+
+        $('#export_excel').on('click', function(e) {
+            e.preventDefault();
+            table.button(2).trigger();
+        });
+
+        $('#export_csv').on('click', function(e) {
+            e.preventDefault();
+            table.button(3).trigger();
+        });
+
+        $('#export_pdf').on('click', function(e) {
+            e.preventDefault();
+            table.button(4).trigger();
+        });
+
+    };
+
+     var initTable1 = function() {
         var table = $('#kt_table_1').DataTable({
             responsive: true,
-            // Pagination settings
+            
             dom: "<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>\
 			<'row'<'col-sm-12'tr>>\
 			<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>",
@@ -145,11 +212,11 @@ var KTDatatablesExtensionButtons = function() {
 						  <i class="la la-ellipsis-h"></i>\
 						</a>\
 						<div class="dropdown-menu dropdown-menu-right">\
-							<a class="dropdown-item" href="/admin/impianti/edit/' + data + '"><i class="la la-edit"></i> Modifica Impianto</a>\
+							<a class="dropdown-item" href="/admin/impianti/edit/' + data + '"><i class="la la-edit"></i> Modifica</a>\
                             <form action="/admin/impianti/' + data + '" method="POST">\
                                 <input type="hidden" name="_method" value="DELETE">\
                                     <a class="dropdown-item">\
-                                        <button type="submit" class="btn btn-danger">Elimina impianto</button>\
+                                        <button type="submit" class="btn btn-danger">Elimina</button>\
                                     </a>\
                             </form>\
 						</div>\
@@ -212,8 +279,13 @@ var KTDatatablesExtensionButtons = function() {
 						  <i class="la la-ellipsis-h"></i>\
 						</a>\
 						<div class="dropdown-menu dropdown-menu-right">\
-							<a class="dropdown-item" href="/admin/society/edit/' + data + '"><i class="la la-edit"></i> Modifica Gestore</a>\
-							<a class="dropdown-item" href="/admin/society/delete/' + data + '"><i class="la la-trash"></i> Elimina Gestore</a>\
+							<a class="dropdown-item" href="/admin/society/edit/' + data + '"><i class="la la-edit"></i> Modifica</a>\
+                            <form action="/admin/society/' + data + '" method="POST">\
+                            <input type="hidden" name="_method" value="DELETE">\
+                                <a class="dropdown-item">\
+                                    <button type="submit" class="btn btn-danger">Elimina</button>\
+                                </a>\
+                        </form>\
 						</div>\
 					</span>';
                 },
@@ -256,6 +328,7 @@ var KTDatatablesExtensionButtons = function() {
             initTable1();
             initTable2();
             initTable3();
+            initTable4();
         }
     };
 }();
